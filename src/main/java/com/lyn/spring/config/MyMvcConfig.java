4c15614804b79c7c2c74afec26438261174d6760
lyn <yunhaipiaodi@126.com>
2020-05-07 08:54:49 +0800

    111

--- src/main/java/com/lyn/spring/config/MyMvcConfig.java
+++ src/main/java/com/lyn/spring/config/MyMvcConfig.java
@@ -13,7 +13,5 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MyMvcConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("index");
-        registry.addViewController("/index.html").setViewName("index");
     }
 }
--- src/main/java/com/lyn/spring/controller/HelloController.java
+++ src/main/java/com/lyn/spring/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.lyn.spring.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
  * @author lyn
  * @create 2020-05-05 10:49
  */
-@RestController
+@Controller
 public class HelloController {
+
+    @RequestMapping("/test")
+    public String index(){
+        return "index";
+    }
+
 
     @RequestMapping("/hello")
     public String Hello(){
