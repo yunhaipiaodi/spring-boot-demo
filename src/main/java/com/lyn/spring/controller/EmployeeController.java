96705caea66487187992b517f34dcbafb9bf9ce2
lyn <yunhaipiaodi@126.com>
2020-05-08 14:11:32 +0800

    1,增加高亮选择显示； 2，添加“添加雇员”模块

--- src/main/java/com/lyn/spring/controller/EmployeeController.java
+++ src/main/java/com/lyn/spring/controller/EmployeeController.java
@@ -1,11 +1,14 @@
 package com.lyn.spring.controller;
 
+import com.lyn.spring.dao.DepartmentDao;
 import com.lyn.spring.dao.EmployeeDao;
+import com.lyn.spring.entities.Department;
 import com.lyn.spring.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Collection;
 
@@ -19,10 +22,27 @@ public class EmployeeController {
     @Autowired
     EmployeeDao employeeDao;
 
+    @Autowired
+    DepartmentDao departmentDao;
+
     @GetMapping("/emps")
     public String list(Model model){
         Collection<Employee> all = employeeDao.getAll();
         model.addAttribute("emps",all);
         return "/emp/list";
+    }
+
+    @GetMapping("/emp")
+    public String add(Model model){
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("departments",departments);
+        return "/emp/add";
+    }
+
+    @PostMapping("/emp")
+    public String submit(Employee employee){
+        employeeDao.save(employee);
+        //提交后，重定向到list页面
+        return "redirect:/emps";
     }
 }
